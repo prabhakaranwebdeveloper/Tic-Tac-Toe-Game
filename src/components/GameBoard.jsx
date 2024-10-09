0d79082987015dca455ca57928eff8eb793af5e9
prabhakaranwebdeveloper <prabhakaranwebdeveloper@gmail,com>
2024-10-09 11:20:14 +0530

    first commit

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,22 @@
+export default function GameBoard({ OnSelectSquare, board }) {
+  return (
+    <ol id="game-board">
+      {board.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li key={colIndex}>
+                <button
+                  onClick={() => OnSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
